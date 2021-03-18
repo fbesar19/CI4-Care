ffbde576a75d69c6556dc7fd8f6500e61eed0deb
fbesar <fbesar19@gmail.com>
2021-03-18 20:15:21 +0700

    Init

---
+++ app/Views/home.php
@@ -0,0 +1,274 @@
+<html>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>CARE</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="css/salingbantu.css">
+    <style>
+        * {
+            font-family: 'Poppins', sans-serif;
+        }
+
+        .baner {
+            background-image: linear-gradient(to right,
+                    rgba(26, 37, 66, 0.7),
+                    rgba(106, 140, 231, 0.7)),
+                url("background-1.jpg");
+            background-size: cover;
+            background-position: center;
+        }
+
+        .carousel-item img {
+            height: 50vh;
+            object-fit: cover;
+        }
+
+        .carousel-item img {
+            height: 50vh;
+            object-fit: cover;
+        }
+
+        .foto-bencana {
+            height: 180px;
+            object-fit: cover;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!-- navigasi -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white shadow-sm">
+        <div class="container">
+            <a class="navbar-brand" href="">
+                <img src="icon.png" alt="" width="30" height="29" class="d-inline-block align-top">
+                <strong class="text-primary">CARE</strong>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Beranda">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#Penggalangan-Dana">Penggalangan Dana</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#Testimoni">Testimoni</a>
+                    </li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- beranda -->
+    <div class="container-fluid bg-primary py-5 baner" id="Beranda">
+        <div class="container py-5">
+            <div class="col-md-4 text-white">
+                <h1 class="text-white">#temanBERDASI</h1>
+                <hr class="border border-4 border-white" width="150">
+                <p>#temanBERDASI merupakan program yang diadakan oleh Suncode.id lewat CARE untuk membantu mereka-mereka yang terdampak bencana dan membutuhkan uluran tangan.</p>
+                <button class="btn border border-2 border-white text-white rounded-pill mt-3 px-4">Mulai Berdonasi</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- deskripsi -->
+    <div class="container-fluid bg-white py-5 my-5" data-aos="fade-up" data-aos-duration="1000">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 py-5">
+                    <h3 class="text-primary">Kenapa harus donasi di CARE??</h3>
+                    <span>Berikut alasan kenapa kamu harus ikut donasi di CARE</span>
+                </div>
+                <div class="col-md-3 bg-light text-center p-4">
+                    <img src="mudah.png" height="60" class="mt-3" alt="">
+                    <h5 class="mt-2">Mudah</h5>
+                    <p class="text-muted">Untuk berdonasi dapat dilakun secara online atau datang ke poskoBERDASI</p>
+                </div>
+                <div class="col-md-3 bg-white text-center p-4 shadow" style="z-index: 3;">
+                    <img src="transparan.png" height="60" class="mt-3" alt="">
+                    <h5 class="mt-2">Transparan</h5>
+                    <p class="text-muted">Dana yang sudah tersalurkan akan ada dokumentasi nya langsung di web CARE</p>
+                </div>
+                <div class="col-md-3 text-center p-4">
+
+                </div>
+                <div class="col-md-3 bg-light text-center p-4">
+                    <img src="tersampaikan.png" height="60" class="mt-3" alt="">
+                    <h5 class="mt-2">Tersampaikan</h5>
+                    <p class="text-muted">Dana yang didonasikan sudah pasti tersampaikan kepada yang dituju</p>
+                </div>
+                <div class="col-md-3 bg-white text-center p-4 shadow" style="z-index: 3;">
+                    <img src="jelas.png" height="60" class="mt-3" alt="">
+                    <h5 class="mt-2">Jelas</h5>
+                    <p class="text-muted">Setiap penggalangan dana memiliki progress yang dapat dilihat di web CARE</p>
+                </div>
+                <div class="col-md-3 bg-light text-center p-4">
+                    <img src="cepat.png" height="60" class="mt-3" alt="">
+                    <h5 class="mt-2">Cepat</h5>
+                    <p class="text-muted">Apabila dana sudah terkumpul akan langsung didistribusikan</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tertimoni -->
+    <div class="container text-center py-4 mb-4" data-aos="fade-up" data-aos-duration="1000">
+        <div class="row">
+            <div class="col-md-6 px-0">
+                <div id="carouselExampleControls" class="slide carousel-fade" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="background-1.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="anak-kecil.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="anak-yatim.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 p-5 text-start">
+                <h3 class="text-primary mt-5">Mereka yang sudah bisa tersenyum lagi karena bantuan <u>#temanBERDASI</u></h3>
+            </div>
+        </div>
+    </div>
+
+    <!-- penggalangan donasi -->
+    <div class="container py-4" data-aos="fade-up" data-aos-duration="1000" id="Penggalangan-Dana">
+        <h3 class="text-primary text-center">Mereka Yang Butuh Bantuan</h3>
+        <div class="row py-4">
+            <div class="col-md-3 my-3">
+                <div class="card shadow-sm rounded">
+                    <img src="banjir-kalsel.jpg" class="card-img-top foto-bencana" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Bantu Korban Banjir Kalimantan</h5>
+                        <hr>
+                        <table width="100%">
+                            <tr>
+                                <td>Sisa Waktu</td>
+                                <td align="right">Terkumpul</td>
+                            </tr>
+                            <tr>
+                                <td><b>187 hari</b></td>
+                                <td align="right"><b>Rp. 150.000,00</b></td>
+                            </tr>
+                        </table>
+                        <div class="progress mt-3">
+                            <div class="progress-bar w-50" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-3">
+                <div class="card shadow-sm rounded">
+                    <img src="gempa-bumi.jpeg" class="card-img-top foto-bencana" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Bantu Korban Gempa Bumi Sulawesi</h5>
+                        <hr>
+                        <table width="100%">
+                            <tr>
+                                <td>Sisa Waktu</td>
+                                <td align="right">Terkumpul</td>
+                            </tr>
+                            <tr>
+                                <td><b>30 hari</b></td>
+                                <td align="right"><b>Rp. 225.000,00</b></td>
+                            </tr>
+                        </table>
+                        <div class="progress mt-3">
+                            <div class="progress-bar w-75" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-3">
+                <div class="card shadow-sm rounded">
+                    <img src="longsor-cililin.jpg" class="card-img-top foto-bencana" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Bantu Korban Longsor Cililin</h5>
+                        <hr>
+                        <table width="100%">
+                            <tr>
+                                <td>Sisa Waktu</td>
+                                <td align="right">Terkumpul</td>
+                            </tr>
+                            <tr>
+                                <td><b>7 hari</b></td>
+                                <td align="right"><b>Rp. 321.000,00</b></td>
+                            </tr>
+                        </table>
+                        <div class="progress mt-3">
+                            <div class="progress-bar w-100" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-3">
+                <div class="card shadow-sm rounded">
+                    <img src="pembangunan-jembatan.jpg" class="card-img-top foto-bencana" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text">Bantu Pembangunan Jembatan Putus</h5>
+                        <hr>
+                        <table width="100%">
+                            <tr>
+                                <td>Sisa Waktu</td>
+                                <td align="right">Terkumpul</td>
+                            </tr>
+                            <tr>
+                                <td><b>148 hari</b></td>
+                                <td align="right"><b>Rp. 33.000,00</b></td>
+                            </tr>
+                        </table>
+                        <div class="progress mt-3">
+                            <div class="progress-bar w-25" role="progressbar" aria-valuenow="5" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid bg-primary p-5 text-center baner">
+        <span class="text-white">Ingin menjadi #temanBERDASI?</span> <button class="btn btn-light rounded-pill text-primary mx-3">Daftar Sekarang</button>
+    </div>
+    <!-- footer -->
+
+    <footer id="sticky-footer" class="py-2 bg-primary text-white">
+        <div class="container text-center">
+            <small>CARE ~ Copyright &copy;2021</small>
+        </div>
+    </footer>
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+</html>
